ba62fabad9755925eade3f77bf2db968dc75504b
GophTheGreat <13459026+GophTheGreat@users.noreply.github.com>
2023-06-30 16:28:35 -1000

    add tests for isbalanced rebalance and delete

--- main.js
+++ main.js
@@ -18,8 +18,11 @@ let testarray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 testarray.sort(ascending);
 console.log(testarray)
 let treeA = new Tree(testarray);
-
 prettyPrint(treeA.root)
+
+//test isbalanced
+console.log(`TEST ISBALANCED (true)`);
+console.log(treeA.isBalanced())'
 
 //test insert
 console.log("TEST INSERT(11)")
@@ -30,6 +33,29 @@ console.log("TEST INSERT(7)")
 treeA.insert(7);
 console.log("TEST INSERT(3333)")
 treeA.insert(3333);
+console.log("TEST INSERT(3334)")
+treeA.insert(3334);
+console.log("TEST INSERT(3335)")
+treeA.insert(3335);
+prettyPrint(treeA.root)
+
+//test isbalanced
+console.log(`TEST ISBALANCED (false)`);
+console.log(treeA.isBalanced())'
+
+//test rebalance
+console.log(`TEST REBALANCE`);
+treeA.rebalance();
+prettyPrint(treeA.root)
+console.log(`CHECK ISBALANCED (true)`);
+console.log(treeA.isBalanced())'
+
+//test delete
+console.log(`TEST DELETE EXISTING (1)`);
+treeA.delete(1);
+prettyPrint(treeA.root)
+console.log(`TEST DELETE NONEXISTING (8675)`);
+treeA.delete(8675);
 prettyPrint(treeA.root)
 
 //test inorder
